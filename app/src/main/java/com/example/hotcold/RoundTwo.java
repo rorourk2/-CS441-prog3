e25363cf81d89a2990592cc49e09ce94cf8f9cff
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-06-28 13:02:59 -0400

    screen 2

--- app/src/main/java/com/example/hotcold/RoundTwo.java
+++ app/src/main/java/com/example/hotcold/RoundTwo.java
@@ -7,6 +7,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RoundTwo extends AppCompatActivity {
     int temp;
@@ -19,10 +22,30 @@ public class RoundTwo extends AppCompatActivity {
         Intent mIntent=getIntent();
         temp=mIntent.getIntExtra("the",0);
 
+        final TextView t=(TextView) findViewById(R.id.textView);
+        String L=Integer.toString(temp);
+        L="It is "+L+"°C";
+        t.setText(L);
+
+        ImageView I1=findViewById(R.id.imageView2);
+        ImageView I2=findViewById(R.id.imageView3);
+        ImageView I3=findViewById(R.id.imageView4);
+        ImageView I4=findViewById(R.id.imageView5);
+
+
+        if(temp<4){
+            I1.setVisibility(View.VISIBLE);
+        }else if(temp<17){
+            I2.setVisibility(View.VISIBLE);
+        }else if(temp<30){
+            I3.setVisibility(View.VISIBLE);
+        }else{
+            I4.setVisibility(View.VISIBLE);
+        }
 
 
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        Button fab = findViewById(R.id.button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
